a05da4ebaaa489b97fd664d060703da64dc65f81
kharioki <antolisah@gmail.com>
2020-08-28 00:20:20 +0300

    added data.ts

--- src/data.ts
+++ src/data.ts
@@ -8,5 +8,63 @@ export const data = {
     linkedin: '',
     objective: 'To build cool tech products.'
   },
-  skills: []
+  skills: ['Javascript', 'Wood-working', 'Fashion accessorizing'],
+  positions: [
+    {
+      id: 1,
+      title: 'Engineering Lead',
+      company: 'Cango Africa',
+      startDate: '2019-03-01',
+      endDate: '2020-03-05',
+      employmentType: 'FULL_TIME',
+      location: 'Kinshasa, DRC',
+      achievements: [
+        'Developing of React web dashboard application for the management and marketing team',
+        'Building the react-native riders and drivers mobile applications, working with GraphQL, Nest, Kafka and RideOS',
+        'Reviewing tech team hiring, translating business plans to the engineering team'
+      ]
+    },
+    {
+      id: 2,
+      title: 'Mobile developer',
+      company: 'Mookh Africa',
+      startDate: '2018-06-01',
+      endDate: '2018-11-05',
+      employmentType: 'FULL_TIME',
+      location: 'Nairobi, Kenya',
+      achievements: [
+        'Developing of React web dashboard application for the management and marketing team',
+        'Building the react-native riders and drivers mobile applications, working with GraphQL, Nest, Kafka and RideOS',
+        'Reviewing tech team hiring, translating business plans to the engineering team'
+      ]
+    },
+    {
+      id: 3,
+      title: 'Web developer',
+      company: 'Sinbad Technologies',
+      startDate: '2018-01-01',
+      endDate: '2018-06-12',
+      employmentType: 'FULL_TIME',
+      location: 'Nairobi, Kenya',
+      achievements: [
+        'Developing of React web dashboard application for the management and marketing team',
+        'Building the react-native riders and drivers mobile applications, working with GraphQL, Nest, Kafka and RideOS',
+        'Reviewing tech team hiring, translating business plans to the engineering team'
+      ]
+    },
+    {
+      id: 4,
+      title: 'Entrepreneur in Training',
+      company: 'MEST Africa',
+      startDate: '2016-08-15',
+      endDate: '2017-08-17',
+      employmentType: 'FULL_TIME',
+      location: 'Accra, Ghana',
+      achievements: [
+        'Developing of React web dashboard application for the management and marketing team',
+        'Building the react-native riders and drivers mobile applications, working with GraphQL, Nest, Kafka and RideOS',
+        'Reviewing tech team hiring, translating business plans to the engineering team'
+      ]
+    }
+  ]
 };
